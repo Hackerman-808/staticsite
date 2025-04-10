7d9e9f6fff7fdb1b1aaec0a5fe32a28360b355ad
Hackerman-808 <addressforitstuff@gmail.com>
2025-04-10 18:59:36 +0200

    added (under great pain) LeafNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_1(self):
@@ -19,6 +19,27 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(None, 5, "know", {"<what>": "https://www.to.com", 
                                           "put": "_here!"})
         self.assertEqual(node.__repr__(), "HTMLNode(None, 5, know, {'<what>': 'https://www.to.com', 'put': '_here!'})")
+
+
+    def test_leaf_to_html_tag_as_none_value(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+
+    def test_leaf_to_html_tag_value_as_none(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
+    def test_leaf_to_html_tag_value(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+
+    def test_leaf_to_html_tag_value_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
 
 
 if __name__ == "__main__":
