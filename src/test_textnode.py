7e82c014e1bebef80bc3c3f0400f2bfb975d3399
Hackerman-808 <addressforitstuff@gmail.com>
2025-05-12 17:08:35 +0200

    Added extractors for Markdown images and links. I hate regex.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestTextNode(unittest.TestCase):
     def test_eq_text_type_url_as_none(self):
@@ -157,6 +157,98 @@ class TestTextNode(unittest.TestCase):
             TextNode(" more text ", TextType.TEXT),
             TextNode("more code", TextType.CODE)])
 
+
+# KEEP IN MIND: Malformed Markdown images or links or other formatting errors are IGNORED -UNLESS- NO valid entries are present.
+    def test_extract_image_empty_input(self):
+        with self.assertRaises(Exception):
+            extract_markdown_images("")
+
+
+    def test_extract_image_mono_input(self):
+        image_res = extract_markdown_images(
+            "This is text with a ![thing](https://i.imgur.com/cambridge.gif)")
+        self.assertEqual(image_res,
+                         [("thing", "https://i.imgur.com/cambridge.gif")])
+
+
+    def test_extract_image_dual_input(self):
+        image_res = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)")
+        self.assertEqual(image_res,
+                         [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+
+    def test_extract_image_bad_format(self):
+        with self.assertRaises(Exception):
+            extract_markdown_images(
+                "This is text with a ![rick roll(https://i.imgur.com/aKaOqIh.gif)")
+
+
+    def test_extract_image_bad_and_valid_format(self):
+    # As stated above, this is not ideal but the check can't see incorrect syntax on multiple entries when one is correct - 
+    # too much work to fix, so leave for now as is.
+        image_res = extract_markdown_images(
+            "This is text with a ![rick roll(https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)")
+        self.assertEqual(image_res,
+                         [("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+
+    def test_extract_image_bad_and_bad_format(self):
+        with self.assertRaises(Exception):
+            extract_markdown_images(
+                "This is text with a ![rick roll(https://i.imgur.com/aKaOqIh.gif) and ![obi wan]https://i.imgur.com/fJRm4Vk.jpeg)")
+
+
+    def test_extract_image_empty_partial(self):
+        with self.assertRaises(Exception):
+            extract_markdown_images(
+                "This is text with a ![](https://i.imgur.com/aKaOqIh.gif)")
+
+
+    def test_extract_link_empty_input(self):
+        with self.assertRaises(Exception):
+            extract_markdown_links("")
+
+
+    def test_extract_link_mono_input(self):
+        link_res = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)")
+        self.assertEqual(link_res,
+                         [("to boot dev", "https://www.boot.dev")])
+
+
+    def test_extract_link_dual_input(self):
+        link_res = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)")
+        self.assertEqual(link_res,
+                         [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+    def test_extract_link_bad_format(self):
+        with self.assertRaises(Exception):
+            extract_markdown_links(
+                "This is text with a link to boot dev](https://www.boot.dev)")
+
+
+    def test_extract_link_bad_and_valid_format(self):
+    # As stated above, this is not ideal but the check can't see incorrect syntax on multiple entries when one is correct - 
+    # too much work to fix, so leave for now as is.
+        link_res = extract_markdown_links(
+            "This is text with a link [to boot dev https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)")
+        self.assertEqual(link_res,
+                         [("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+
+    def test_extract_link_bad_and_bad_format(self):
+        with self.assertRaises(Exception):
+            extract_markdown_links(
+                "This is text with a link [to boot dev https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev")
+
+
+    def test_extract_link_empty_partial(self):
+        with self.assertRaises(Exception):
+            extract_markdown_links(
+                "This is a link [](https://www.youtube.com/@bootdotdev)")
+            
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -48,33 +48,34 @@ def text_node_to_html_node(text_node):
 				return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
 
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-# Note: This function assumes that appropriate delimiters are used for the given text_type.
-# It doesn't validate the semantic correctness of delimiter/text_type pairs.
-	if not delimiter:
-		raise Exception("No delimiter provided.")
-	if not isinstance(text_type, TextType):
-		raise Exception("No valid text type provided.")
-	new_nodes = []
+# def split_nodes_delimiter(old_nodes, delimiter, text_type):
+# # THIS IS THE OLD VERSION, KEPT FOR ARCHIVAL PURPOSES!
+# # Note: This function assumes that appropriate delimiters are used for the given text_type.
+# # It doesn't validate the semantic correctness of delimiter/text_type pairs.
+# 	if not delimiter:
+# 		raise Exception("No delimiter provided.")
+# 	if not isinstance(text_type, TextType):
+# 		raise Exception("No valid text type provided.")
+# 	new_nodes = []
 	
-	for node in old_nodes:
-		if not isinstance(node.text_type, TextType):
-			raise Exception("No valid text type provided.")
-		elif node.text_type != TextType.TEXT:
-			new_nodes.append(node) 
-		elif delimiter not in node.text:
-			new_nodes.append(node)
-		elif node.text.count(delimiter) % 2 != 0:
-			raise Exception("There are unclosed delimiters present. Please verify correct format.")
-		else:
-			split_node = node.text.split(delimiter)
+# 	for node in old_nodes:
+# 		if not isinstance(node.text_type, TextType):
+# 			raise Exception("No valid text type provided.")
+# 		elif node.text_type != TextType.TEXT:
+# 			new_nodes.append(node) 
+# 		elif delimiter not in node.text:
+# 			new_nodes.append(node)
+# 		elif node.text.count(delimiter) % 2 != 0:
+# 			raise Exception("There are unclosed delimiters present. Please verify correct format.")
+# 		else:
+# 			split_node = node.text.split(delimiter)
 
-			for entry in split_node:
-				if entry[0].isspace() or entry[-1].isspace():
-					new_nodes.append(TextNode(entry, TextType.TEXT))
-				elif not entry[0].isspace() and entry[-1].isspace():
-					new_nodes.append(TextNode(entry, text_type))
-	return new_nodes
+# 			for entry in split_node:
+# 				if entry[0].isspace() or entry[-1].isspace():
+# 					new_nodes.append(TextNode(entry, TextType.TEXT))
+# 				elif not entry[0].isspace() and entry[-1].isspace():
+# 					new_nodes.append(TextNode(entry, text_type))
+# 	return new_nodes
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -110,5 +111,38 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return new_nodes
 
 
+def input_valid_for_extractors(findallres):
+# Malformed Markdown images or links or other formatting errors are IGNORED unless NO valid entries are present.
+	bad_res = []
+	gud_res = []
+	
+	if len(findallres) == 0:
+		raise Exception(
+			f"\n	Input was improperly formatted.\n"
+			"	Please provide correct Markdown syntax with both text and link in the correct format."
+			)
+	
+	for tup in findallres:
+		if len(tup[0]) == 0 or len(tup[1]) == 0:
+			bad_res.append(tup)
+		else:
+			gud_res.append(tup)
+
+	if len(bad_res) > 0:
+		raise Exception(
+			f"\n	Input contained the following invalid entries:\n"
+			f"	{bad_res}\n"
+			"	Please provide correct Markdown syntax with both text and link in the correct format."
+			)
+	else:
+		return gud_res
+
+
 def extract_markdown_images(text):
-	pass
+	found_items = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+	return input_valid_for_extractors(found_items)
+	
+
+def extract_markdown_links(text):
+	found_items = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+	return input_valid_for_extractors(found_items)
